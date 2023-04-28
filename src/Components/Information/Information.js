efcdb5cc76a0d2dfb52ae3a511d6734c8b55fb99
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-04-28 18:56:29 +0530

    profile information basic structure

--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import styles from '../../Asssets/CSS/Components/Information/information.module.css'
+
 const Information = () => {
     return (
-        <div>Information</div>
+        <div className={`${styles.information__main}`}>
+            <div className={`${styles.information__inner}`}>
+                <div className={`${styles.information__inner__left}`}>
+                    <div className={`${styles.information__general}`}>
+                        General
+                    </div>
+                    <div className={`${styles.information__company}`}>
+                        Company
+                    </div>
+                </div>
+                <div className={`${styles.information__inner__right}`}>
+                    <div className={`${styles.information__address}`}>
+                        Address
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
